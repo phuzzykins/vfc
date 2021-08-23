3b6900d7be714589da3623dc8a86b84fa6b3d82f
Matthew Ferson <matthew.ferson@gmail.com>
2021-08-23 00:23:27 -0400

    Add splash screen

--- fresh-install.sh
+++ fresh-install.sh
@@ -11,6 +11,8 @@ echo > ~/startvnc "#! /bin/bash"
 echo >> ~/startvnc "x0vncserver -passwordfile /home/pi/.vnc/passwd -display :0 >/dev/null 2>&1 &"
 chmod +x ~/startvnc
 
+sudo apt install fbi -y
+
 cd ~
 git clone https://github.com/th33xitus/kiauh.git
 git clone https://github.com/phuzzykins/udev-media-automount.git
@@ -18,7 +20,17 @@ git clone https://github.com/phuzzykins/vfc.git
 
 ln -s vfc/klipper_config klipper_config
 ln -s vfc/vfc-klipperscreen.conf KlipperScreen.conf
-#echo "[include vfc/vfc-klipperscreen.conf]" > KlipperScreen.conf
+
+cd ~/vfc
+sudo cp splashscreen.service /etc/systemd/system/splashscreen.service
+sudo systemctl enable splashscreen
+sudo systemctl disable getty@tty1
+
+# Add to /boot/cmdline.txt
+# logo.nologo consoleblank=0 loglevel=1 quiet
+
+# Add to /boot/config.txt
+# disable_splash=1
 
 cd ~/udev-media-automount
 sudo make
